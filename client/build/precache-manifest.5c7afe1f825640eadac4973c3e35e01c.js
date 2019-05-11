621aa57fb72a294823656c731352418a39467a67
soc33 <soc33@msn.com>
2019-05-11 01:03:35 -0400

    making it deploy ready for heroku

---
+++ client/build/precache-manifest.5c7afe1f825640eadac4973c3e35e01c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737f993adc7174f0e458dd59c92aa350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9389aa4e28eb5854e60e",
+    "url": "/static/css/main.09623674.chunk.css"
+  },
+  {
+    "revision": "33895440dda0eb0f3d34",
+    "url": "/static/js/2.f70ae7b7.chunk.js"
+  },
+  {
+    "revision": "9389aa4e28eb5854e60e",
+    "url": "/static/js/main.be69f9c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "05dbc5e3c2806d6d22d7c83e6869019b",
+    "url": "/static/media/Parchment.05dbc5e3.jpg"
+  },
+  {
+    "revision": "49c95d326739783fed146d9612e1bb00",
+    "url": "/static/media/book2.49c95d32.png"
+  },
+  {
+    "revision": "aa2d9f844738344f6c06b5e7ba974728",
+    "url": "/static/media/footerImg.aa2d9f84.png"
+  },
+  {
+    "revision": "9aa679d559e2ee2c381505f5e46e75cc",
+    "url": "/static/media/scribble-transparent-cloud-2.9aa679d5.png"
+  }
+]);
